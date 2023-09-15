509d4ee81a151a24bf85cb69cebcb2cd001617c1
Devin Ma <devin200011@gmail.com>
2023-09-15 13:12:34 -0400

    Start more validation for spike detector

--- spikes/spike_detector.py
+++ spikes/spike_detector.py
@@ -140,6 +140,85 @@ def multi_channel_requirement(gdf, nchs, fs):
         return np.array([])
 
 
+def multi_channel_requirement_with_label(gdf, nchs, fs):
+    # Need to change so that it returns total spike counter to help remove duplicates
+    min_chs = 2
+    if nchs < 16:
+        max_chs = np.inf
+    else:
+        max_chs = np.ceil(nchs / 2)
+    min_time = 100 * 1e-3 * fs
+
+    # Check if there is even more than 1 spiking channel. Will throw error
+    try:
+        if len(np.unique(gdf[:, 1])) < min_chs:  # check unique channels
+            return np.array([])
+    except IndexError:
+        return np.array([])
+
+    final_spikes = []
+
+    s = 0  # start at time negative one for 0 based indexing
+    curr_seq = [s]
+    last_time = gdf[s, 0]
+    spike_count = 0
+    while s < (gdf.shape[0] - 1):  # check to see if we are at last spike
+        # move to next spike time
+        new_time = gdf[s + 1, 0]  # calculate the next spike time
+
+        # if it's within the time diff
+        if (
+            new_time - last_time
+        ) < min_time:  # check that the spikes are within the window of time
+            curr_seq.append(s + 1)  # append it to the current sequence
+
+            if s == (
+                gdf.shape[0] - 2
+            ):  # see if you just added the last spike, if so, done with sequence
+                # done with sequence, check if the number of involved chs is
+                # appropriate
+                l = len(np.unique(gdf[curr_seq, 1]))
+                if l >= min_chs and l <= max_chs:
+                    final_spikes.append(
+                        np.hstack(
+                            (
+                                gdf[curr_seq, :2],
+                                np.ones((len(curr_seq), 1)) * spike_count,
+                                gdf[curr_seq, 2:],
+                            )
+                        )
+                    )
+        else:
+            # done with sequence, check if the length of sequence is
+            # appropriate
+            l = len(np.unique(gdf[curr_seq, 1]))
+            if (l >= min_chs) & (l <= max_chs):
+                final_spikes.append(
+                    np.hstack(
+                        (
+                            gdf[curr_seq, :2],
+                            np.ones((len(curr_seq), 1)) * spike_count,
+                            gdf[curr_seq, 2:],
+                        )
+                    )
+                )
+                spike_count += 1
+            # reset sequence
+            curr_seq = [s + 1]
+        # increase the last time
+        last_time = gdf[s + 1, 0]
+
+        # increase the current spike
+        s += 1
+
+    if len(final_spikes) != 0:
+        return np.vstack(
+            final_spikes
+        )  # return all final spikes with a spike sequence counter
+    else:
+        return np.array([])
+
+
 def process_channel(
     signal, fs, tmul, absthresh, sur_time, too_high_abs, spkdur, lpf1, hpf
 ):
@@ -391,12 +470,6 @@ def spike_detector(data, fs, electrode_labels, **kwargs):
         gdf = gdf[close_mask, :]
 
     print("Returning early to skip multi_channel_requirement")
-    return gdf
-
-    # Check that spike occurs in multiple channels
-    if gdf.any() & (nchs > 1):
-        gdf = multi_channel_requirement(gdf, nchs, fs)
-
     return gdf
 
 
